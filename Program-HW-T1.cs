6a6d929ae18449a0892bb75588a461d05a14491e
Michael <wermian@mail.ru>
2022-07-31 17:40:40 +0300

    Init

---
+++ Program-HW-T1.cs
@@ -0,0 +1,11 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитать сколько чисел больше 0 ввел пользователь.
+Console.WriteLine("Введите необходимое количество чисел");
+int countM=Convert.ToInt32(Console.ReadLine());
+int count=0;
+for (int i=0; i<countM; i++)
+{
+    Console.Write($"Введите {i}-ое число:");
+    int number=Convert.ToInt32(Console.ReadLine());
+    if (number>0) count++;
+}
+Console.WriteLine($"Количество чисел больше нуля равно {count}");
